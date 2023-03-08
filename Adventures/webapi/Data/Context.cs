1a81626c264ea7c5b009122d539aeda458e1e2f2
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2023-03-08 18:47:48 +0200

    User class was added and context for SQL connection

---
+++ Adventures/webapi/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Data.Entities;
+
+namespace webapi.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> context) : base(context)
+        {
+        }
+        public DbSet<User> UserLogin { get; set; }
+    }
+}
--- Adventures/webapi/Program.cs
+++ Adventures/webapi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Register the Database context service for dependency injection
+builder.Services.AddDbContext<Context>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connString"));
+});
 
 var app = builder.Build();
 
